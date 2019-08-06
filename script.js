8836e5aa7dfc8ca198ba22fbf3cc84968100ff3e
Enchan <enchant0130@gmail.com>
2019-08-07 07:11:38 +0900

    update slash key to focus search box

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.onkeydown = function(){
+    //--ページ最上段スクロール
     if(event.keyCode == 190) window.scrollTo(0, 0);
+
+    //--検索結果のページでは、/キーで検索ボックスに移動
+    if(location.host == "www.google.com"){
+        if(event.keyCode == 191) {
+            setTimeout(() => {
+                document.forms[0][1].focus();
+            }, 100);
+        }
+    }
 }
