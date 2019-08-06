c3100690fb5e073ef445af55e1569a1a7d563051
Enchan <enchant0130@gmail.com>
2019-08-07 07:23:28 +0900

    update became able to use it on more search engine.

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ document.onkeydown = function(){
     if(event.keyCode == 190) window.scrollTo(0, 0);
 
     //--検索結果のページでは、/キーで検索ボックスに移動
-    if(location.host == "www.google.com"){
-        if(event.keyCode == 191) {
+    if(event.keyCode == 191) {
+        //--Googleの場合
+        if(location.host == "www.google.com"){
             setTimeout(() => {
                 document.forms[0][1].focus();
+            }, 100);
+        }
+
+        //--yahooの場合
+        if(location.host == "www.yahoo.co.jp"){
+            setTimeout(() => {
+                document.forms[1][1].focus();
             }, 100);
         }
     }
